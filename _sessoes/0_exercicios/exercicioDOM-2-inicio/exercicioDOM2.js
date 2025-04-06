c7332d55774ddc0d3b43468a0a509d8022579611
Zesenem <zesenemproductions@hotmail.com>
2025-04-06 16:52:31 +0100

    mudancas exedom2 gerar lis dinamicamente

--- _sessoes/0_exercicios/exercicioDOM-2-inicio/exercicioDOM2.js
+++ _sessoes/0_exercicios/exercicioDOM-2-inicio/exercicioDOM2.js
@@ -31,19 +31,39 @@ let books = [
   },
 ];
 
-let listItems = document.querySelectorAll("section li");
+// primeira versao
+// let listItems = document.querySelectorAll("section li");
 
-for (let i = 0; i < books.length; i++) {
-  const book = books[i];
-  const li = listItems[i];
+// for (let i = 0; i < books.length; i++) {
+//   const book = books[i];
+//   const li = listItems[i];
+//   const readIcon = book.alreadyRead ? "✔️" : "❌";
+
+//   li.innerHTML = `
+//       <p class="book-title">${book.title}</p>
+//       <h2 class="book-author">${book.author}</h2>
+//       <img src="${book.imageUrl}" alt="${book.title}" class="book-image">
+//       <p class="read-status">Already read: <span>${readIcon}</span></p>
+//     `;
+
+//   li.classList.add(book.alreadyRead ? "read" : "unread");
+// }
+
+// segunda versao com forEach e a criação de li's
+
+const bookList = document.getElementById("book-list");
+
+books.forEach((book) => {
+  const li = document.createElement("li");
+
   const readIcon = book.alreadyRead ? "✔️" : "❌";
 
-  li.innerHTML = `  
-      <p class="book-title">${book.title}</p>  
-      <h2 class="book-author">${book.author}</h2>   
-      <img src="${book.imageUrl}" alt="${book.title}" class="book-image">
-      <p class="read-status">Already read: <span>${readIcon}</span></p>
-    `;
+  li.innerHTML = `
+        <p class="book-title">${book.title}</p>  
+        <h2 class="book-author">${book.author}</h2>   
+        <img src="${book.imageUrl}" alt="${book.title}" class="book-image">
+        <p class="read-status">Already read: <span>${readIcon}</span></p>
+      `;
 
-  li.classList.add(book.alreadyRead ? "read" : "unread");
-}
+  bookList.appendChild(li);
+});
